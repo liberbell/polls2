1eeba72741de50db1adfc35f86203a7795e00205
liberbell <liberbell@gmail.com>
2018-09-09 10:25:35 +0900

    url4

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+# add polls url
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('kanri/', admin.site.urls),
